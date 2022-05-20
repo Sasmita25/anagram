403ef733f9b0d1084bd45685f8e8e99c18c94b44
Sasmita25 <104830324+Sasmita25@users.noreply.github.com>
2022-05-20 16:24:37 +0530

    Create cases.py

---
+++ cases.py
@@ -0,0 +1,7 @@
+import pytest
+from anagram import anagram
+assert anagram("EARTH", "HEART") == "Both strings form a Anagram."
+assert anagram("tom", "Mmot") == "Both strings do not form as a Anagram."
+assert anagram({1,3,5},{5,3,1}) == "Both strings form a Anagram."
+assert anagram("E$h*","*hE$") == "Both strings form a Anagram."
+assert anagram({'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6},{'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6}) == "Both strings form a Anagram."
