04e222851bb65b5dfe6b776160619fc30e29abff
Sasmita25 <104830324+Sasmita25@users.noreply.github.com>
2022-05-22 13:40:37 +0530

    Update cases.py

--- cases.py
+++ cases.py
@@ -6,3 +6,5 @@ assert anagram("tom", "Mmot") == "Both strings do not form as a Anagram."
 assert anagram({1,3,5},{5,3,1}) == "Both strings form a Anagram."
 assert anagram("E$h*","*hE$") == "Both strings form a Anagram."
 assert anagram({'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6},{'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6}) == "Both strings form a Anagram."
+if __name__ == '__main__':
+  test_run_stuff()
