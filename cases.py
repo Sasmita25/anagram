d5bea1fce808c789e5735863738fcf2710852ec4
Sasmita25 <104830324+Sasmita25@users.noreply.github.com>
2022-05-23 15:20:24 +0530

    Update cases.py

--- cases.py
+++ cases.py
@@ -1,5 +1,14 @@
-from anagram import anagram
-
+def anagram(string1, string2):
+    print("Before Sort",string1,string2)
+    print("After Sort",sorted(string1),sorted(string2))
+# Strings are sorted and verified
+    if(sorted(string1) == sorted(string2)):
+        
+        print("Both strings form a Anagram.")
+    else:
+      
+       print("Both strings do not form as a Anagram.")
+        
 def test_run_stuff():
         assert anagram("EARTH", "HEART") == "Both strings form a Anagram."
         assert anagram({1,3,5},{5,3,1}) == "Both strings form a Anagram."
