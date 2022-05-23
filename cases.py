8af3e6bb2f3ff5b663d9d184e4471f21eab8400c
Sasmita25 <104830324+Sasmita25@users.noreply.github.com>
2022-05-23 14:30:35 +0530

    Update cases.py

--- cases.py
+++ cases.py
@@ -2,7 +2,6 @@ from anagram import anagram
 
 def test_run_stuff():
         assert(anagram("EARTH", "HEART") == "Both strings form a Anagram.")
-#assert(anagram("tom", "Mmot") == "Both strings do not form as a Anagram.")
         assert(anagram({1,3,5},{5,3,1}) == "Both strings form a Anagram.")
         assert(anagram("E$h*","*hE$") == "Both strings form a Anagram.")
         assert(anagram({'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6},{'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6}) == "Both strings form a Anagram.")
